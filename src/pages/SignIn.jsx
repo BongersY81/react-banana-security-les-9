3bddc26f805ceec9011699b26d56177ee78cda98
yvonnebongers <yvonnebongers@gmail.com>
2026-02-05 21:53:11 +0100

    opdrachten gemaakt

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
-import authContext, {AuthContext} from "../context/AuthContext";
+import {AuthContext} from "../context/AuthContext";
 
 
 function SignIn() {
